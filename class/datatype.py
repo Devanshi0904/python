71064d58f6c8fbe170e8aeb79038519b877f5d47
devu0904 <laddevanshi@gmail.com>
2025-08-12 12:26:32 +0530

    all

---
+++ class/datatype.py
@@ -0,0 +1,29 @@
+#numeric
+a=10
+b=2.5
+c=4j+5
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+# sequnce datatype
+name ="Devanshi"
+subjects = {"python","java","php"}
+t = ("a","b","c",1,2,3)
+
+print(type(name))
+print(type(subjects))
+print(type(t))
+
+#map data type
+s={"test","tech","a","b"}
+m={"python":"1","java":"2"}
+
+print(type(s))
+print(type(m))
+
+
+#boolean
+b= True
+print(type(b))
--- class/demo.py
+++ class/demo.py
@@ -1,1 +1,24 @@
 print("hello world")
+
+print('hello " python')
+
+print("hello ' python ")
+
+# MULTI LINE PRINT
+print(""" hello
+    python
+    hello
+    tops
+""")
+
+#SEPRAT
+print("hello","java","hello","python",sep=" | ")
+
+print()
+
+
+print('hello \\" python')
+
+
+a= input("enter number ")
+print(a)2
