8e10134eb1fcf3e222a4267c09e0a16af70c114c
devu0904 <laddevanshi@gmail.com>
2025-08-19 12:41:23 +0530

    add

---
+++ task/prime_number.py
@@ -0,0 +1,15 @@
+
+for j in range(3,100):
+    prime_number=j
+
+    flag = 0
+
+    for i in range(2,prime_number):
+        if prime_number%i==0:
+            flag = 1
+            break
+    if flag==0:
+        print(f"{prime_number} prime")
+    else:
+        pass
+       
