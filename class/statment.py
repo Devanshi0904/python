07aeb1da5fd3226303af52a71d7a248ed6bc5f3c
devu0904 <laddevanshi@gmail.com>
2025-08-21 10:18:23 +0530

    add

---
+++ class/statment.py
@@ -0,0 +1,80 @@
+## ==========conditional========
+#===== if-else====
+
+
+# age = 15
+# if age>=18 :
+#     print("elegeble for voting")
+# else:
+#     print("not elegeble...")
+
+
+a  =1000000
+b = 20000
+c = 520
+
+# if a>b:
+#     print("A is greater")
+# else : 
+#     print("B is greater")
+
+# if a>b:
+#     if a>c :
+#         print("A is greater")
+#     else:
+#         print("C is greater")
+# else : 
+#     if b>c :
+#         print("B is greater")
+#     else :
+#         print("C is greater")
+
+
+#===========else if==============
+
+if a>b and a>c:
+    print("A is greater")
+elif b>a and b>c:
+    print("B is greater")
+elif c>a and c>b:
+    print("c is greater")
+
+
+#============= for loop ===============
+
+
+# for i in range(15):
+#     print(i)
+
+
+# for i in range(5,15):
+#     print(i)
+
+
+# for i in range(1,15,4):
+#     print(i)
+
+# for i in range(15,1,-2):
+#     print(i)
+
+
+# ============ while ============
+
+# i = 10
+
+# while i<20:
+#     print(i)
+#     i = i+1
+
+# ============= break , continue , pass =============
+
+# for i in range(15):
+#     if i==5:
+#         # break
+#         continue
+#     print(i)
+
+if 10<20:
+    print("20 is greater")
+else:
+    pass
