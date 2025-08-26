0467df94acd78b839c55dabc0c5fde9a301bae5c
devu0904 <laddevanshi@gmail.com>
2025-08-26 10:55:20 +0530

    add

--- task/patern.py
+++ task/patern.py
@@ -1,3 +1,4 @@
+# print("=========== squre ==========")
 # for j in range(5):
 #         print("*"*5)
 
@@ -8,23 +9,107 @@
 #     print()
 
 
-print("============= left up ===========")
+# print("============= left up ===========")
 
-for i in range(5):
-    print("*"*(i+1))
+# for i in range(5):
+#     print("*"*(i+1))
 
-print("============ left down ============")
+# print("============ left down ============")
 
-for i in range (5,0,-1):
-    print("*"*i)
+# for i in range (5,0,-1):
+#     print("*"*i)
 
-print("==========right down ==========")
+# print("==========right down ==========")
 
-for i in range (5,0,-1):
-    print(' '*(5-i)+'*'*i)
+# for i in range (5,0,-1):
+#     print(' '*(5-i)+'*'*i)
 
-print("============ right up =========")
+# print("============ right up =========")
 
-for i in range (1,6):
-    print(' '*(5-i)+'*'*i)
+# for i in range (1,6):
+#     print(' '*(5-i)+'*'*i)
 
+
+# print("============ up tectangel ============")
+# for i in range (1,6):
+#     print(' '*(5-i)+'* '*i)
+
+
+# print("======== down tectengel ==============")
+# for i in range (5,0,-1):
+#     print(' '*(5-i)+'* '*i)
+
+
+
+
+# print("========= odd num triangel ======== ")
+# for i in range (5):
+#     for k in range (5-i):
+#       print(" ",end=" ")
+#     for j in range ((i*2)+1): 
+#       print("*",end=" ")
+#     print()
+
+
+
+# print("========= dimond ===========")
+# for i in range (5):
+#     print(' '*(5-i),'* '*(i+1))
+# for i in range (4,0,-1):
+#     print(' '*(4-i+2),'* '*i)
+
+
+# print("============ hollow dimond ===========")
+
+
+# for i in range(5):     #i=1,2,3,4,5
+#     for j in range( 5 - i ):    #4,3,2,1,0 space
+#         print(" ", end="")
+#     for k in range(i+1):     
+#         if k == 0 or k == (i):
+#             print("* ",end="")
+#         else:
+#             print("  ", end="")  
+#     print()
+# for i in range(4, 0, -1):     #i=4,3,2,1
+#     for j in range( 4 - i + 2):
+#         print(" ", end="")
+#     for k in range(i):
+#         if k == 0 or k== (i - 1): 
+#             print("* ", end="")
+#         else:
+#             print("  ", end="") 
+#     print()
+
+
+# print("======== 1,12,123,1234..... left up =========")
+
+# for i in range(5):
+#     for j in range(i+1):
+#         print(j+1,end="")
+#     print()
+
+
+# print("========= 1,2,3,4,.....left up ========== ")
+# num=1
+# for i in range(1,6):
+#     for j in range(1,i+1 ):
+#         print(num,end="")
+#         num += 1
+#     print()
+
+
+# print("=========== 5,54,543,5432,..... left up")
+
+# for i in range (5,0,-1):
+#     for j in range(i,6):
+#         print(j,end=" ")
+#     print()
+
+
+# print("======== 0,10,010,1010,.....left up ===========")
+
+for i in range (5):
+    for j in range(i):
+        print(j+i,end=" ")
+    print()
