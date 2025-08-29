18c3a80ea19f23dbe0ab18bb8f3097eceda76f97
tisha <tisha@gmail.com>
2025-08-29 16:45:25 +0530

    add

---
+++ task/string.py
@@ -0,0 +1,17 @@
+st = "my name is devanshi"
+# print(len(st))
+# print(str.lower(st))
+# print(str.upper(st))
+# print(str.capitalize(st))
+# print(str.title(st))
+# print(str.strip(st))
+
+
+# old = "devanshi"
+# new = "tisha"
+# count = "20"
+# print(str.replace(count,new,old))
+
+substring = "devanshi"
+print(str.find(substring))
+
