5c264100f39cf1b52d0975950da7a4ba8817cf72
tisha <tisha@gmail.com>
2025-09-13 09:17:00 +0530

    add

---
+++ class/dictionary.py
@@ -0,0 +1,15 @@
+st = {
+    "name":"devanshi",
+    "email":"devanshi@gmail.com",
+    "language":["guj","hindi","eng"]
+}
+# print(st)
+# print(st['name'])
+# print(st.get('name'))
+
+# st['name'] = "nilay"
+# print(st)
+
+st.update({"subject":"python","stream":"IT"})
+print(st)
+# print(st['language'])
--- class/set.py
+++ class/set.py
@@ -33,3 +33,39 @@ a = {"a","b","c","d",100,1}
 
 x = {1,2,3,4,5}
 y = {4,5,6,7,8,True}
+
+# z = x.union(y)
+# print(z)
+
+# x.update(y)
+# print(x)
+
+# z = x|y
+# print(z)
+
+# z = x.intersection(y)
+# print(z)
+
+# z = x-y
+# print(z)
+
+# x.difference_update(y)
+# print(x)
+
+# z = x.symmetric_difference(y)
+# print(z)
+
+# z = x^y
+# print(z)
+
+# x.symmetric_difference_update(y)
+# print(x)
+
+
+k = {10,20,30,40,50}
+j = {100,200}
+
+# print(j.issubset(k))
+# print(k.issuperset(j))
+
+# print(j.isdisjoint(k))
