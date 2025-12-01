8eec42a4e78206c06e59ab92aea3b377db309837
tisha <tisha@gmail.com>
2025-12-01 09:32:52 +0530

    add

---
+++ project/myapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from myapp.views import *
+
+urlpatterns = [
+path("",index,name="index"),
+path("cart",cart,name="cart"),
+path("chackout",chackout,name="chackout"),
+path("contact",contact,name="contact"),
+path("shop",shop,name="shop"),
+]
